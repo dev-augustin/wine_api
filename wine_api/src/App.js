165e7569e39d968a6cbfea0eb586490bf2ae3dcc
dev-augustin <dev.augustin00@gmail.com>
2020-04-11 18:03:29 -0500

    Designed Menu display

--- wine_api/src/App.js
+++ wine_api/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import './App.css';
 
 const API_URL=process.env.REACT_APP_API_URL;
 console.log(API_URL)
@@ -23,44 +24,52 @@ export default class App extends Component {
           //console.log("Response: ", response.data);
           const dataName=response.data;
           console.log("test",dataName.length);
-          let wineNamesArray=[];
-          let wineImagesArray=[];
-          for (let i=0; i< dataName.length; i++){
-            wineNamesArray.push(dataName[i].name);
-            //console.log("Wine: ", wineNamesArray);
-            wineImagesArray.push(dataName[i].picture);
-           // console.log("ImageUrl: ", wineImagesArray);
-          }
-        
-        let imagesList=  wineImagesArray.map((image, index) =>( 
-            <img src= {image} key={index} alt ="" />
-            ))
+            let winePic=dataName.map((winename, index)=>(
+            <ul className="ulIndex"><li className="listIndex" key={index}>{winename.name} <img src={winename.picture} alt = "wine names" height='100px'/> </li>
+            </ul>));
+          // let winePic=dataName.map((winename, index)=>(<p className="ulIndex" key={index}>{winename.name} <img src={winename.picture} alt = "hi"/> </p>));
+          //  console.log("WinePic: ",winePic)
+        //   let wineNamesArray=[];
+        //   let wineImagesArray=[];
+        //   for (let i=0; i< dataName.length; i++){
+        //     wineNamesArray.push(dataName[i].name);
+        //     //console.log("Wine: ", wineNamesArray);
+        //     wineImagesArray.push(dataName[i].picture);
+        //    // console.log("ImageUrl: ", wineImagesArray);
+        //   }
+     
+        // let imagesList=  wineImagesArray.map((image, index) =>( 
+        //     <img src= {image} key={index} alt ="" />
+        //     ))
 
           //console.log(imagesList);
 
-          let mergeResult= imagesList.reduce(function(mergeResult, field, index){
-            mergeResult[wineNamesArray[index]]=field;
-            return mergeResult;
-          }, {})
-          console.log("mergeResult: ", mergeResult)
+          // let mergeResult= imagesList.reduce(function(mergeResult, field, index){
+          //   mergeResult[wineNamesArray[index]]=field;
+          //   return mergeResult;
+          // }, {})
+          // console.log("mergeResult: ", mergeResult)
         // this.setState({wineNames: wineNamesArray});
         // this.setState({winePictures: imagesList});
         // this.setState({wineNamePicList: [wineNamesArray, imagesList]})
-        this.setState({wineNamePicList: mergeResult})
-
+//setState({wineNamePicList: mergeResult})
+this.setState({wineNamePicList:winePic})
        }
        catch(e){
          console.error(e);
        }
   }
+
+
+ 
   render() {
     return (
       <React.Fragment>
 
-      <div>
+      <div className="display" >
 
-        {/* {this.state.wineNamePicList.map(listItem => <div> {})
-        } */}
+        {this.state.wineNamePicList}
+        }
         </div>
  
 
@@ -79,3 +88,5 @@ export default class App extends Component {
 //https://stackoverflow.com/questions/56783262/how-to-store-data-from-json-api-response-into-array-in-reactjs
 //React: Displaying images from api call - https://stackoverflow.com/questions/54314689/react-displaying-images-from-api-call
 //Merge two array as key value pair - https://riptutorial.com/javascript/example/8628/merge-two-array-as-key-value-pair
+//Rendering an Array of Data with map() and JSX
+//- http://www.hackingwithreact.com/read/1/13/rendering-an-array-of-data-with-map-and-jsx
