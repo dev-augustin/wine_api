4502dd1ddab92f9ccb71d5e1d0f9fd97fdbec8cc
dev-augustin <dev.augustin00@gmail.com>
2020-04-11 03:01:18 -0500

    wine names display

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# wine_api
+#### Objectives:
+
+1. Render the wines in the webpage.
+2. When someone clicks a wine display information of that single wine in the webpage. HTTP GET method.
+3. Make a form to create new wines. HTTP POST method.
+4. Make an option to delete a chosen wine. - HTTP DELETE method.
+5. Your app has to use React and Axios
--- wine_api/src/App.js
+++ wine_api/src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_URL=process.env.REACT_APP_API_URL;
+console.log(API_URL)
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      name: []
+    }
+  }
+  componentDidMount(){
+    this.getWineApi();
+  }
+
+  async getWineApi(){
+       try{
+          const response = await axios.get(API_URL);
+          //console.log(API_URL);
+          console.log("Response: ", response.data);
+          const dataName=response.data;
+          console.log(dataName[0]);
+          this.setState({
+            name:dataName.map((winename,i) => 
+            (<li key={i} className='wine-name-list'>{winename.name}</li>))
+          });
+        }
+       catch(e){
+         console.error(e);
+       }
+  }
+  render() {
+    return (
+      <div>
+      <ul className="wine-name-list">{this.state.name}</ul>
+      </div>
+    );
+  }
 }
 
-export default App;
+
+//Reference: To Display the array in list - https://stackoverflow.com/questions/47658765/objects-are-not-valid-as-a-react-child-found-object-promise
